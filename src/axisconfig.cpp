74190eddb5003477332c1b144487137a1cc9d016
Wearily <parsapournabi1382@gmail.com>
2026-06-25 19:34:35 +0330

    [ui] create AxisConfig class to manage Axis Scaling and other configs
    registered: AxisConfig class as QML component.

---
+++ src/axisconfig.cpp
@@ -0,0 +1,55 @@
+#include "../include/InjectionMolding/axisconfig.h"
+
+AxisConfig::AxisConfig(QObject* parent)
+    : QObject{parent},
+      m_numerator(1),
+      m_denominator(1),
+      m_zeroPUU(0),
+      m_maxDecimals(0)
+{
+
+}
+
+double AxisConfig::toUnit(qint32 puu) const
+{
+    return static_cast<double>(puu - m_zeroPUU) * m_denominator / m_numerator;
+}
+
+qint32 AxisConfig::toPUU(double unit) const
+{
+    double doublePUU = unit * m_numerator / m_denominator + m_zeroPUU;
+    return static_cast<qint32>(qBound(static_cast<qint64>(INT32_MIN),
+                                      qRound64(doublePUU),
+                                      static_cast<qint64>(INT32_MAX)));
+}
+
+void AxisConfig::setZeroPUU(qint32 zeroPUU)
+{
+    m_zeroPUU = zeroPUU;
+
+    emit gearRatioChanged();
+}
+
+void AxisConfig::setScale(qint64 numerator, qint64 denominator)
+{
+    m_numerator = qMin(numerator, 1LL);
+    m_denominator = qMin(denominator, 1LL);
+
+    applyMaxDecimals();
+
+    emit gearRatioChanged();
+}
+
+void AxisConfig::applyMaxDecimals()
+{
+    double resolution = static_cast<double>(m_numerator) / static_cast<double>(m_denominator);
+
+    int d = 0;
+
+    while (resolution < 1.0)
+    {
+        resolution *= 10.0;
+        ++d;
+    }
+    m_maxDecimals = d;
+}
