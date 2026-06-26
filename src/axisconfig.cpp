98887b2c960c69fcaef2b05f7bcf88ce828fa67c
Wearily <parsapournabi1382@gmail.com>
2026-06-27 00:02:30 +0330

    [ui] add AxisConfig properties into the AppSettings read/write automatically
    added: objectName for each AxisConfig instances which is required for AppSettings ordering.

--- src/axisconfig.cpp
+++ src/axisconfig.cpp
@@ -1,13 +1,23 @@
 #include "../include/InjectionMolding/axisconfig.h"
 
+#include <QSettings>
+#include <QGuiApplication>
+
+#define GET_SETTING_NAME(x) objectName() + "_" + QString(#x)
+
 AxisConfig::AxisConfig(QObject* parent)
-    : QObject{parent},
-      m_numerator(1),
-      m_denominator(1),
-      m_zeroPUU(0),
-      m_maxDecimals(0)
+    : QObject{parent}
+{
+}
+
+void AxisConfig::classBegin()
 {
 
+}
+
+void AxisConfig::componentComplete()
+{
+    readFromSettings();
 }
 
 double AxisConfig::toUnit(qint32 puu) const
@@ -49,9 +59,48 @@ void AxisConfig::syncronize(AxisConfig* other)
     m_decimals = other->m_decimals;
     m_unitName = other->m_unitName;
 
+    writeToSettings();
+
+    updateAll();
+}
+
+void AxisConfig::updateAll()
+{
     emit unitNameChanged();
     emit decimalsChanged();
     emit gearRatioChanged();
+}
+
+void AxisConfig::readFromSettings()
+{
+    QSettings settings(QSettings::IniFormat,
+                       QSettings::UserScope,
+                       QGuiApplication::organizationName(),
+                       QGuiApplication::applicationName());
+
+    m_numerator = settings.value(GET_SETTING_NAME(NUMERATOR), 1LL).toLongLong();
+    m_denominator = settings.value(GET_SETTING_NAME(DENOMINATOR), 1LL).toLongLong();
+    m_zeroPUU = settings.value(GET_SETTING_NAME(ZERO_OFFSET), 0).toInt();
+    m_maxDecimals = settings.value(GET_SETTING_NAME(MAX_DECIMALS), 0).toInt();
+    m_decimals = settings.value(GET_SETTING_NAME(DECIMALS), 0).toInt();
+    m_unitName = settings.value(GET_SETTING_NAME(UNIT_NAME), "PUU").toString();
+
+    updateAll();
+}
+
+void AxisConfig::writeToSettings() const
+{
+    QSettings settings(QSettings::IniFormat,
+                       QSettings::UserScope,
+                       QGuiApplication::organizationName(),
+                       QGuiApplication::applicationName());
+
+    settings.setValue(GET_SETTING_NAME(NUMERATOR), m_numerator);
+    settings.setValue(GET_SETTING_NAME(DENOMINATOR), m_denominator);
+    settings.setValue(GET_SETTING_NAME(ZERO_OFFSET), m_zeroPUU);
+    settings.setValue(GET_SETTING_NAME(MAX_DECIMALS), m_maxDecimals);
+    settings.setValue(GET_SETTING_NAME(DECIMALS), m_decimals);
+    settings.setValue(GET_SETTING_NAME(UNIT_NAME), m_unitName);
 }
 
 void AxisConfig::applyMaxDecimals()
