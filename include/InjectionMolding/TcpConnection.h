c8dcd5ec131ae93c37571f08bbaf05333055af23
Wearily <parsapournabi1382@gmail.com>
2026-07-01 02:18:20 +0330

    [ui] add connected property for TcpConnection class

--- include/InjectionMolding/TcpConnection.h
+++ include/InjectionMolding/TcpConnection.h
@@ -8,6 +8,7 @@ class TcpConnection : public QObject
         Q_OBJECT
         W_PROP_HDEF(QString, host, Host, "192.168.1.1");
         W_PROP_HDEF(int, port, Port, 502);
+        W_PROP_HDEF(bool, connected, Connected, false);
     public:
         explicit TcpConnection(QObject* parent = nullptr)
             : QObject{parent}
