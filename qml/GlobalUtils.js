6766f86a628f2c1220a165d5325b7579632562ac
Wearily <parsapournabi1382@gmail.com>
2026-07-12 14:48:59 +0330

    [ui] add toolTip colors array into the GlobalUtils js

--- qml/GlobalUtils.js
+++ qml/GlobalUtils.js
@@ -1,5 +1,20 @@
 .pragma library
 
+const toolTipBackgroundColor = ["#00000000" // 0 Transparent
+                                , "#CC121212" // 1 Glass
+                                , "#C4111111" // 2 Soft
+                                , "#C41E1E1E" // 3 Soft
+                                , "#F0202020" // 4 Default
+                                , "#F0282828" // 5 Elevated
+                                , "#FF303030"  // 6 Active
+      ];
+
+const toolTipBorderColor = ["#00000000", "#AA8A4B00", "orange", "#DDAA6600", "#EEBB7700", "#FFFF9800"];
+
+const toolTipContentColor = ["#777777", "#A0A0A0", "#C0C0C0", "#D8D8D8", "#E8E8E8", "#FFFFFF"];
+
+const toolTipContentHoverColor = ["#A0A0A0", "#CFCFCF", "#E0E0E0", "#FFFFFF", "#FFFFB74D", "#FFFF9800"];
+
 const indicatorActiveLevels = {
     red: 0,
     yellow: 1,
