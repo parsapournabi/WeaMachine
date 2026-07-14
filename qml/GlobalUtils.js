7776f4d67ea1d0c195cae9137524369f718c4b02
Wearily <parsapournabi1382@gmail.com>
2026-07-14 16:00:58 +0330

    [ui] updating toolTip colors on GlobalUtils.js

--- qml/GlobalUtils.js
+++ qml/GlobalUtils.js
@@ -2,14 +2,14 @@
 
 const toolTipBackgroundColor = ["#00000000" // 0 Transparent
                                 , "#CC121212" // 1 Glass
-                                , "#C4111111" // 2 Soft
-                                , "#C41E1E1E" // 3 Soft
+                                , "#F4111111" // 2 Soft
+                                , "#F41E1E1E" // 3 Soft
                                 , "#F0202020" // 4 Default
                                 , "#F0282828" // 5 Elevated
                                 , "#FF303030"  // 6 Active
       ];
 
-const toolTipBorderColor = ["#00000000", "#AA8A4B00", "orange", "#DDAA6600", "#EEBB7700", "#FFFF9800"];
+const toolTipBorderColor = ["#00000000", "#AA8A4B00", "orange", "orange", "#EEBB7700", "#FFFF9800"];
 
 const toolTipContentColor = ["#777777", "#A0A0A0", "#C0C0C0", "#D8D8D8", "#E8E8E8", "#FFFFFF"];
 
