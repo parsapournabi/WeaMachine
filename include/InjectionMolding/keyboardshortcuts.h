e6969232e9ec9a8cdf7487aa782e52c3a18ce2c7
Wearily <parsapournabi1382@gmail.com>
2026-08-30 18:53:09 +0330

    [dev] add required functions for KeyboardShortcuts class
    defined: functions logic on KeyboardShortcuts class.

--- include/InjectionMolding/keyboardshortcuts.h
+++ include/InjectionMolding/keyboardshortcuts.h
@@ -3,13 +3,37 @@
 
 #include <QObject>
 
+class KeyboardShortcutItem;
 class KeyboardShortcuts : public QObject
 {
-    Q_OBJECT
-public:
-    explicit KeyboardShortcuts(QObject *parent = nullptr);
+        Q_OBJECT
+        Q_PROPERTY(int count READ count NOTIFY countChanged)
+    public:
+        explicit KeyboardShortcuts(QObject* parent = nullptr);
 
-signals:
+        /** QML Interface SLOTS **/
+        Q_INVOKABLE bool add(KeyboardShortcutItem item);
+        Q_INVOKABLE bool remove(int index);
+        Q_INVOKABLE KeyboardShortcutItem* get(int index);
+        Q_INVOKABLE const KeyboardShortcutItem* get(int index) const;
+
+        Q_INVOKABLE bool exists(const KeyboardShortcutItem& item) const;
+        Q_INVOKABLE bool exists(const QKeySequence& sequence) const;
+        Q_INVOKABLE bool isValid(const KeyboardShortcutItem& item) const;
+        Q_INVOKABLE bool isValid(const QKeySequence& item) const;
+        Q_INVOKABLE bool isAllValid() const;
+
+        Q_INVOKABLE bool empty() const;
+        Q_INVOKABLE bool isEmpty() const;
+        Q_INVOKABLE int count() const;
+        Q_INVOKABLE int size() const;
+        Q_INVOKABLE int length() const;
+
+    signals:
+        void countChanged();
+
+    private:
+        QVector<KeyboardShortcutItem> m_buffer;
 };
 
 #endif // KEYBOARDSHORTCUTS_H
--- src/keyboardshortcuts.cpp
+++ src/keyboardshortcuts.cpp
@@ -1,5 +1,124 @@
 #include "../include/InjectionMolding/keyboardshortcuts.h"
+#include "../include/InjectionMolding/KeyboardShortcutItem.h"
+
+#include <QDebug>
 
 KeyboardShortcuts::KeyboardShortcuts(QObject* parent)
     : QObject{parent}
-{ }
+{
+}
+
+bool KeyboardShortcuts::add(KeyboardShortcutItem item)
+{
+    if (!item.isValid())
+    {
+        qWarning() << __FUNCTION__ << "Item isn't valid: " << item.name() << item.keySequence();
+        return false;
+    }
+
+    if (exists(item))
+    {
+        qWarning() << __FUNCTION__ << "Item is alread exists: " << item.name() << item.keySequence();
+        return false;
+    }
+
+    m_buffer.append(item);
+
+    emit countChanged();
+    return true;
+}
+
+bool KeyboardShortcuts::remove(int index)
+{
+    if (index < 0 || index >= m_buffer.size())
+    {
+        qWarning() << __FUNCTION__ << "Invalid Index value: " << index;
+        return false;
+    }
+
+    m_buffer.remove(index);
+    emit countChanged();
+    return true;
+}
+
+KeyboardShortcutItem* KeyboardShortcuts::get(int index)
+{
+    if (index < 0 || index >= m_buffer.size())
+    {
+        qWarning() << __FUNCTION__ << "Invalid Index value: " << index;
+        return nullptr;
+    }
+
+    return &m_buffer[index];
+}
+
+const KeyboardShortcutItem* KeyboardShortcuts::get(int index) const
+{
+    if (index < 0 || index >= m_buffer.size())
+    {
+        qWarning() << __FUNCTION__ << "Invalid Index value: " << index;
+        return nullptr;
+    }
+
+    return &m_buffer.at(index);
+}
+
+bool KeyboardShortcuts::exists(const KeyboardShortcutItem& item) const
+{
+    return exists(item.keySequence());
+}
+
+bool KeyboardShortcuts::exists(const QKeySequence& sequence) const
+{
+    return std::any_of(m_buffer.cbegin(),
+                       m_buffer.cend(),
+                       [&](const auto & keyboardShortcutItem)
+    {
+        return sequence == keyboardShortcutItem.keySequence();
+    });
+}
+
+bool KeyboardShortcuts::isValid(const KeyboardShortcutItem& item) const
+{
+    return KeyboardShortcutItem::isValid(item);
+}
+
+bool KeyboardShortcuts::isValid(const QKeySequence& item) const
+{
+    return KeyboardShortcutItem::isValid(item);
+}
+
+bool KeyboardShortcuts::isAllValid() const
+{
+    return std::all_of(m_buffer.cbegin(),
+                       m_buffer.cend(),
+                       [](const auto & item)
+    {
+        return item.isValid();
+    });
+}
+
+bool KeyboardShortcuts::empty() const
+{
+    return m_buffer.empty();
+}
+
+bool KeyboardShortcuts::isEmpty() const
+{
+    return m_buffer.isEmpty();
+}
+
+int KeyboardShortcuts::count() const
+{
+    return m_buffer.count();
+}
+
+int KeyboardShortcuts::size() const
+{
+    return m_buffer.size();
+}
+
+int KeyboardShortcuts::length() const
+{
+    return m_buffer.length();
+}
